f922c28b8b18dec05770552e81dee7ad25d6bfe3
Gabriel <prismaticnetwork12@gmail.com>
2022-08-17 20:15:50 -0300

    feat: adding new route to userId through params

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -7,7 +7,10 @@ const HomeController = (req, res, next) => {
 //post method /login
 const HomeLogin = (req, res, next) => {
   const { username, password } = req.body;
-  if (!username && !password) return;
+  if (!username && !password)
+    return res
+      .status(400)
+      .json({ error: "password and username does not match" });
   res.send({
     username,
     password,
@@ -15,4 +18,17 @@ const HomeLogin = (req, res, next) => {
   });
 };
 
-module.exports = { HomeController, HomeLogin };
+//getting the userId through params
+const UserId = (req, res, next) => {
+  const { id } = req.params;
+  const { username, password } = req.body;
+  if (!id && !username && !password)
+    return res.status(404).json({ error: "User not found" });
+  res.json({
+    user: username,
+    pass: password,
+    id,
+  });
+};
+
+module.exports = { HomeController, HomeLogin, UserId };
--- Routes/HomeRoutes.js
+++ Routes/HomeRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const { HomeController, HomeLogin } = require("../Controllers/homeController");
+const {
+  HomeController,
+  HomeLogin,
+  UserId,
+} = require("../Controllers/homeController");
 const HomeRouter = express.Router();
 HomeRouter.get("/", HomeController);
 HomeRouter.post("/login", HomeLogin);
+HomeRouter.get("/user/:id", UserId);
 module.exports = HomeRouter;
